616e4ac9cd4d26164d57fe201785cd3e4acdbd1b
Heiffeng <blacard@163.com>
2023-04-27 18:49:54 +0800

    修改包名

--- src/main/java/site/achun/learning/LearningApplication.java
+++ src/main/java/site/achun/learning/LearningApplication.java
@@ -1,13 +1,13 @@
-package site.achun.springbootlearning;
+package site.achun.learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootLearningApplication {
+public class LearningApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootLearningApplication.class, args);
+        SpringApplication.run(LearningApplication.class, args);
     }
 
 }
